4e03a3dcd1cb51a5c9888b39908cd6f03e0b1ef7
Rutvik Korat <korat.rutvik2511@gmail.com>
2025-08-30 11:41:07 +0530

    Phase 3.2.1

---
+++ lib/models/site_agency_model.dart
@@ -0,0 +1,87 @@
+import '../models/category_model.dart';
+
+class SiteAgencyModel {
+  final int id;
+  final int siteId;
+  final String? categoryName;
+  final int categoryId;
+  final String name;
+  final String mobile;
+  final String email;
+  final String createdAt;
+  final String updatedAt;
+  final String? deletedAt;
+  final CategoryModel? category;
+
+  SiteAgencyModel({
+    required this.id,
+    required this.siteId,
+    this.categoryName,
+    required this.categoryId,
+    required this.name,
+    required this.mobile,
+    required this.email,
+    required this.createdAt,
+    required this.updatedAt,
+    this.deletedAt,
+    this.category,
+  });
+
+  factory SiteAgencyModel.fromJson(Map<String, dynamic> json) {
+    return SiteAgencyModel(
+      id: json['id'] ?? 0,
+      siteId: json['site_id'] ?? 0,
+      categoryName: json['category_name'],
+      categoryId: json['category_id'] ?? 0,
+      name: json['name'] ?? '',
+      mobile: json['mobile'] ?? '',
+      email: json['email'] ?? '',
+      createdAt: json['created_at'] ?? '',
+      updatedAt: json['updated_at'] ?? '',
+      deletedAt: json['deleted_at'],
+      category: json['category'] != null 
+          ? CategoryModel.fromJson(json['category']) 
+          : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'site_id': siteId,
+      'category_name': categoryName,
+      'category_id': categoryId,
+      'name': name,
+      'mobile': mobile,
+      'email': email,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+      'deleted_at': deletedAt,
+      'category': category?.toJson(),
+    };
+  }
+}
+
+
+
+class SiteAgencyResponse {
+  final String status;
+  final String message;
+  final List<SiteAgencyModel> data;
+
+  SiteAgencyResponse({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+
+  factory SiteAgencyResponse.fromJson(Map<String, dynamic> json) {
+    return SiteAgencyResponse(
+      status: json['status'] ?? '',
+      message: json['message'] ?? '',
+      data: (json['data'] as List<dynamic>?)
+          ?.map((item) => SiteAgencyModel.fromJson(item))
+          .toList() ?? [],
+    );
+  }
+}
